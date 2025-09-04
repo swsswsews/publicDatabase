87b6ae071b18b84fe7c6e06a44e87ca6a5108137
swsswsews <CHENGFENG07.24@ichat.sp.edu.sg>
2025-09-04 15:03:17 +0800

    add user and database routes

--- codes/back-end/server/routes/mainRoute.js
+++ codes/back-end/server/routes/mainRoute.js
@@ -4,9 +4,11 @@ const router=express.Router()
 
 /*routes*/
 //user
+const userRoutes=require("./userRoute")
 router.use("/user", userRoutes)
 
 //database
+const databaseRoutes=require("./databaseRoute")
 router.use("/database", databaseRoutes)
 /*======*/
 
