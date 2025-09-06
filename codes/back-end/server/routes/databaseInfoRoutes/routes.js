90c150fde56b926f44304a9e7feda509b6c0b5ec
swsswsews <CHENGFENG07.24@ichat.sp.edu.sg>
2025-09-06 21:52:08 +0800

    plan database route structure

---
+++ codes/back-end/server/routes/databaseInfoRoutes/routes.js
@@ -0,0 +1,18 @@
+//load express router
+const express=require("express")
+const router=express.Router()
+
+/*routes*/
+
+//create database
+
+//update database
+
+//delete database
+
+//get information in database
+
+/*======*/
+
+//export
+module.exports=router
