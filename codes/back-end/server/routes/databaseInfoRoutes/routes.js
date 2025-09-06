d8d7770ee3649334d39c4578a585faa7d5b7eb50
swsswsews <CHENGFENG07.24@ichat.sp.edu.sg>
2025-09-07 02:57:30 +0800

    compelete databaseInfo main route

--- codes/back-end/server/routes/databaseInfoRoutes/routes.js
+++ codes/back-end/server/routes/databaseInfoRoutes/routes.js
@@ -2,15 +2,21 @@
 const express=require("express")
 const router=express.Router()
 
+const controller=require("../controllers/createDatabaseController")
+
 /*routes*/
 
 //create database
+router.use("/createDatabase",controller.createDatabase)
 
 //update database
+router.use("/updateDatabase",controller.updateDatabase)
 
 //delete database
+router.use("/deleteDatabase",controller.deleteDatabase)
 
 //get information in database
+router.use("/getDatabaseInfo",controller.getDatabaseInfo)
 
 /*======*/
 
