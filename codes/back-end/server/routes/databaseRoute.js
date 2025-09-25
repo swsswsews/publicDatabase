0982d7b19d41c111e0d73b8f91846139cb64dc29
swsswsews <CHENGFENG07.24@ichat.sp.edu.sg>
2025-09-25 17:56:40 +0800

    compelete database route

---
+++ codes/back-end/server/routes/databaseRoute.js
@@ -0,0 +1,54 @@
+//get express router
+const express = require('express');
+const router = express.Router();
+
+//get controller
+const controller = require('../controllers/databases');
+
+//add database
+router.post("/addDatabase", controller.addDatabase)
+
+//get database
+router.get("/getDatabase", controller.getDatabase)
+
+/*
+==========================================================contents management========================================================
+*/
+
+//display editable content
+router.get("/getEditableContent", controller.getEditableContent)
+
+//add table
+router.post("/addTable", controller.editContent.addTable)
+
+//delete table
+router.delete("/deleteTable", controller.editContent.deleteTable)
+
+//add column
+router.post("/addColumn", controller.editContent.addColumn)
+ 
+//delete column
+router.delete("/deleteColumn", controller.editContent.deleteColumn)
+
+//add value
+router.post("/addValue", controller.editContent.addValue)
+ 
+//delete value
+router.delete("/deleteValue", controller.editContent.deleteValue)
+  
+//edit value
+router.put("/editValue", controller.editContent.editValue)
+  
+//get data
+router.get("/getData", controller.getData)
+
+/*
+==========================================================contents management========================================================
+*/
+
+
+//delete database
+router.delete("/deleteDatabase", controller.deleteDatabase)
+
+//export
+module.exports = router;
