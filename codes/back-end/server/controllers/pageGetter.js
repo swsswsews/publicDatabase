c0ca0c1ec3384215a45f2936ed968b0ae3598a19
swsswsews <CHENGFENG07.24@ichat.sp.edu.sg>
2025-10-04 07:35:38 +0800

    change some file to html

--- codes/back-end/server/controllers/pageGetter.js
+++ codes/back-end/server/controllers/pageGetter.js
@@ -1,12 +1,24 @@
 //load home page
 module.exports.loadHomePage = (req, res) => {
-    res.render('home');
+    res.sendFile('../../../front-end/html/homePage.ejs');
 }
 
 //load register page
+module.exports.loadRegisterPage = (req, res) => {
+    res.sendFile('../../../front-end/html/registerPage.ejs');
+}
 
 //load login page
+module.exports.loadLoginPage = (req, res) => {
+    res.sendFile('../../../front-end/html/loginPage.ejs');
+}
 
 //load profile
+module.exports.loadProfilePage = (req, res) => {
+    res.render('../../../front-end/html/profilePage.ejs');
+}
 
 //load databaseInfo page
+module.exports.loadDatabaseInfoPage = (req, res) => {
+    res.render('../../../front-end/html/databaseInfoPage.ejs');
+}
